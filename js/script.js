1dd141dc1074782998c463c8194dc9e385cf70e8
mac-mini <lexx.mg@gmail.com>
2020-11-09 17:34:32 +0300

    first commit

--- js/script.js
+++ js/script.js
@@ -4,98 +4,168 @@ const inputFile = document.querySelector('.form__item');
 const table = document.querySelector('.table__body');
 const link = document.querySelector('.link');
 
-
-
-const abonents = new ParsAbonCsv(abonCsv);
-const mn = new ParsMNcsv(strMN);
-const abc3 = new ParsABC(strABC3);
-const abc4 = new ParsABC(strABC4);
-const abc8 = new ParsABC(strABC8);
-const abc9 = new ParsABC(strABC9);
-const result = [];
+let subscriberData;
+let phonecallsData;
+let mnData;
+let abc3Data;
+let abc4Data;
+let abc8Data;
+let abc9Data;
 
 inputFile.addEventListener('change', () => {
-  const file = inputFile.files[0];
+  const file = inputFile.files;
 
+  for (let elem of file) {
+    if (elem.name === 'phonecallsdata.csv' && !phonecallsData) {
+      readeFile(elem, data => {
+        phonecallsData = data;
+        console.log('callBack phpnecall');
+        check();
+      });
+    } else if (elem.name === 'MN.csv' && !mnData) {
+      readeFile(elem, data => {
+        mnData = data;
+        console.log('callBack MN');
+        check();
+      });
+    }else if (elem.name === 'subscriber.csv' && !subscriberData) {
+      readeFile(elem, data => {
+        subscriberData = data;
+        console.log('callBack subscriber');
+        check();
+      });
+    } else if (elem.name === 'ABC-3xx.csv' && !abc3Data) {
+      readeFile(elem, data => {
+        abc3Data = data;
+        console.log('callBack ABC-3');
+        check();
+      });
+    } else if (elem.name === 'ABC-4xx.csv' && !abc4Data) {
+      readeFile(elem, data => {
+        abc4Data = data;
+        console.log('callBack ABC-4');
+        check();
+      });
+    } else if (elem.name === 'ABC-8xx.csv' && !abc8Data) {
+      readeFile(elem, data => {
+        abc8Data = data;
+        console.log('callBack ABC-8');
+        check();
+      });
+    } else if (elem.name === 'DEF-9xx.csv' && !abc9Data) {
+      readeFile(elem, data => {
+        abc9Data = data;
+        console.log('callBack ABC-9');
+        check();
+      });
+    } else {
+      alert('недопустимое имя выйла');
+    }
+  }
+
+});
+
+function check() {
+  if (phonecallsData && subscriberData && mnData && abc3Data && abc4Data && abc8Data && abc9Data) {
+    console.log('Все файли загружены');
+    // console.log(phonecallsData);
+    // console.log(mnData);
+
+    pars(phonecallsData, subscriberData, mnData, abc3Data, abc4Data, abc8Data, abc9Data);
+    //readeFile(phonecallsData);
+  }
+}
+
+function readeFile(data, callBack) {
   let reader = new FileReader();
-  reader.readAsText(file);
+  reader.readAsText(data);
 
   reader.addEventListener('loadstart', () => {
     console.log('чтение файла начато');
   });
 
-  reader.addEventListener('load', () => {
-    const ats = new ParsATScsv(reader.result);
-    const callOut = ats.callOut;
-
-    for (let obj of callOut) {
-      for (let pref of mn.mn) {
-        if ( isCode(obj['Номер Б'], pref.combine) ) {
-          obj['Оператор'] = pref.nameCountry;
-          obj['Тариф'] = pref.cost;
-          obj['Списание'] = +obj['Длит. (окр.)'] * parseFloat(pref.cost.replace(',', '.') );
-        }
-      }
-
-      for (let abon of abonents.abonents) {
-        if ( isCode(obj['Номер A'], abon.phoneNumber) ) {
-          obj['Абонент'] = abon.bonentName;
-        }
-      }
-
-      if ( isCode(obj['Номер Б'], 3) ) {
-        for (let objABC of abc3.objPref) {
-          if ( +obj['Номер Б'] >= Number(objABC.abc + objABC.start) && +obj['Номер Б'] <= Number(objABC.abc + objABC.end) ) {
-            obj['Оператор'] = objABC.RegionName;
-            break;
-          }
-        }
-      }
-
-      if ( isCode(obj['Номер Б'], 4) ) {
-        for (let objABC of abc4.objPref) {
-          if ( +obj['Номер Б'] >= Number(objABC.abc + objABC.start) && +obj['Номер Б'] <= Number(objABC.abc + objABC.end) ) {
-            obj['Оператор'] = objABC.RegionName;
-            break;
-          }
-        }
-      }
-
-      if ( isCode(obj['Номер Б'], 8) ) {
-        for (let objABC of abc8.objPref) {
-          if ( +obj['Номер Б'] >= Number(objABC.abc + objABC.start) && +obj['Номер Б'] <= Number(objABC.abc + objABC.end) ) {
-            obj['Оператор'] = objABC.RegionName;
-            break;
-          }
-        }
-      }
-
-      if ( isCode(obj['Номер Б'], 9) ) {
-        for (let objABC of abc9.objPref) {
-          if ( +obj['Номер Б'] >= Number(objABC.abc + objABC.start) && +obj['Номер Б'] <= Number(objABC.abc + objABC.end) ) {
-            obj['Оператор'] = objABC.RegionName;
-            break;
-          }
-        }
-      }
-      table.append(createTables(obj));
-    }
-    //console.log(callOut);
-    //console.table(callOut);
-    console.log('докумнт свормирован можно скачивать');
-    document.querySelector('.table').classList.remove('hidden');
-    const csv = arrObjtoCSV(callOut);
-
-    const blob = new Blob(["\ufeff", csv]);
-    const url = URL.createObjectURL(blob);
-    link.href = url;
-    link.download = "data.csv";
+  reader.addEventListener('load', function() {
+    callBack(reader.result);
   });
 
   reader.addEventListener('error', () => {
     console.log('файл не прочитан ' + reader.error);
   });
-});
+}
+
+function pars(phoneData, sub, mN, abc3x, abc4x, abc8x, abc9x) {
+  const ats = new ParsATScsv(phoneData);
+  const subscriber = new ParsAbonCsv(sub);
+  const mn = new ParsMNcsv(mN);
+  const abc3 = new ParsABC(abc3x);
+  const abc4 = new ParsABC(abc4x);
+  const abc8 = new ParsABC(abc8x);
+  const abc9 = new ParsABC(abc9x);
+  const callOut = ats.callOut;
+
+  for (let obj of callOut) {
+    for (let pref of mn.mn) {
+      if ( isCode(obj['Номер Б'], pref.combine) ) {
+        obj['Оператор'] = pref.nameCountry;
+        obj['Тариф'] = pref.cost;
+        obj['Списание'] = +obj['Длит. (окр.)'] * parseFloat(pref.cost.replace(',', '.') );
+      }
+    }
+
+    for (let abon of subscriber.abonents) {
+      if ( isCode(obj['Номер A'], abon.phoneNumber) ) {
+        obj['Абонент'] = abon.bonentName;
+      }
+    }
+
+    if ( isCode(obj['Номер Б'], 3) ) {
+      for (let objABC of abc3.objPref) {
+        if ( +obj['Номер Б'] >= Number(objABC.abc + objABC.start) && +obj['Номер Б'] <= Number(objABC.abc + objABC.end) ) {
+          obj['Оператор'] = objABC.RegionName;
+          break;
+        }
+      }
+    }
+
+    if ( isCode(obj['Номер Б'], 4) ) {
+      for (let objABC of abc4.objPref) {
+        if ( +obj['Номер Б'] >= Number(objABC.abc + objABC.start) && +obj['Номер Б'] <= Number(objABC.abc + objABC.end) ) {
+          obj['Оператор'] = objABC.RegionName;
+          break;
+        }
+      }
+    }
+
+    if ( isCode(obj['Номер Б'], 8) ) {
+      for (let objABC of abc8.objPref) {
+        if ( +obj['Номер Б'] >= Number(objABC.abc + objABC.start) && +obj['Номер Б'] <= Number(objABC.abc + objABC.end) ) {
+          obj['Оператор'] = objABC.RegionName;
+          break;
+        }
+      }
+    }
+
+    if ( isCode(obj['Номер Б'], 9) ) {
+      for (let objABC of abc9.objPref) {
+        if ( +obj['Номер Б'] >= Number(objABC.abc + objABC.start) && +obj['Номер Б'] <= Number(objABC.abc + objABC.end) ) {
+          obj['Оператор'] = objABC.RegionName;
+          break;
+        }
+      }
+    }
+    table.append(createTables(obj));
+  }
+  //console.log(callOut);
+  //console.table(callOut);
+  console.log('докумнт свормирован можно скачивать');
+  document.querySelector('.table').classList.remove('hidden');
+  const csv = arrObjtoCSV(callOut);
+
+  const blob = new Blob(["\ufeff", csv]);
+  const url = URL.createObjectURL(blob);
+  link.href = url;
+  link.download = "data.csv";
+}
 
 function isCode(num, code) {
   const lengthCode = String(code).length;
